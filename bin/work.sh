feb6439c6b66371af5baac473cdc187ef46aaab6
Getty Ritter <gettylefou@gmail.com>
2015-04-22 01:28:34 -0700

    Switched from Pandoc to Python Markdown

--- bin/work.sh
+++ bin/work.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PATH=$DIR:$PATH
 
-pandoc $2/text \
+markdown_py2 $2/text \
   | json-dict contents - \
   | mustache - templates/textpage.mustache \
   | json-dict title "$(cat $2/metadata.yaml | jq '.name')" contents - \
